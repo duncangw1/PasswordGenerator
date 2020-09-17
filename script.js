2a9eed2c69e0231100206071b36f692eeeffd771
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-09-17 12:47:33 -0400

    Replaced if/else statement with while loop containing if/else if/else statement. This is for the user to input the password length.

--- script.js
+++ script.js
@@ -14,21 +14,36 @@ generateBtn.addEventListener("click", writePassword);
 
 // Generate password function
 function generatePassword() {
+  // Variable to set password length
   var passwordLength = parseInt(
     prompt(
       "How long do you want the password to be? Choose between 8 and 128 characters."
     )
   );
-  console.log(passwordLength);
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    alert(
-      "You have chosen a password length of " + passwordLength + " characters."
-    );
-    console.log("valid password length");
-  } else {
-    alert(
-      "Input invalid. You must choose a password length between 8 and 128 characters."
-    );
-    console.log("NOT a valid password length");
+  console.log("users initial choice: " + passwordLength);
+  // While loop confirming the user input between 8 and 128 characters
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    if (passwordLength < 8) {
+      passwordLength = parseInt(
+        prompt(
+          "You chose less than 8 characters. Please choose a length between 8-128 characters."
+        )
+      );
+      console.log("user chose less than 8. New choice: " + passwordLength);
+    } else if (passwordLength > 128) {
+      passwordLength = parseInt(
+        prompt(
+          "You chose more than 128 characters. Please choose a length between 8-128 characters."
+        )
+      );
+      console.log("user chose more than 8. New choice: " + passwordLength);
+    } else {
+      passwordLength = parseInt(
+        prompt(
+          "Password length is required to continue. Please choose a length between 8-128 characters."
+        )
+      );
+      console.log("user chose nothing. New choice: " + passwordLength);
+    }
   }
 }
